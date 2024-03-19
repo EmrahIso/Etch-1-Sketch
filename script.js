44cace22a55e0dc890eff02c32f9ca42d5be8017
Emrah Isovic <emrahisovic20@gmail.com>
2024-03-19 23:39:24 +0100

    Added function to create grid using range parameter. Event Listener for hover effect on grid cells and Changing the input value.

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
 const rootEl = document.querySelector('html');
 
-const etchGridEl = document.querySelector('.etch__grid');
+const rangeInputEl = document.querySelector('.range__input');
+const rangeValueEl = document.querySelector('.range__value');
+
+const etchFlexContEl = document.querySelector('.etch__flex-cont');
+
+rangeInputEl.addEventListener('change', e => {
+    rangeValueEl.textContent = `${e.target.value}x${e.target.value}`;
+})
+
+createGrid(16);
+
+function createGrid(range){
+    for(let i = 1; i <= range; i++) {
+        const etchFlexRowEl = document.createElement('div');
+        etchFlexRowEl.setAttribute('class', 'etch__flex-row');
+        for(let j = 1; j <= range; j++) {
+            const etchBoxEl = document.createElement('div');
+            etchBoxEl.setAttribute('class', 'etch__box');
+            etchFlexRowEl.appendChild(etchBoxEl);
+        }
+        etchFlexContEl.appendChild(etchFlexRowEl);
+    }
+}
+
+const etchFlexContElWidth = etchFlexContEl.offsetWidth;
+etchFlexContEl.style.height = `${etchFlexContElWidth}px`;
+const clearBtnEl = document.querySelector('#clearBtn');
+
+
+etchFlexContEl.addEventListener('mouseover', e => {
+    let eTargetEl = e.target;
+    if(eTargetEl.className === 'etch__box') {
+        eTargetEl.style.backgroundColor = 'var(--color-black)';
+        clearBtnEl.addEventListener('click', e => {
+            eTargetEl.style.backgroundColor = '';
+        })
+    }
+})
+

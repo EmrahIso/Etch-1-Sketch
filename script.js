f42c3a9e36a17db0cb6a97717c7e87946e60ea8e
Emrah Isovic <emrahisovic20@gmail.com>
2024-03-20 23:49:14 +0100

    Functionality to change painting color onclick on colorBox

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ const rangeInputEl = document.querySelector('.range__input');
 const rangeValueEl = document.querySelector('.range__value');
 const rangeBtnEl = document.querySelector('.range-btn');
 
-console.log(rangeBtnEl);
-
 const etchFlexContEl = document.querySelector('.etch__flex-cont');
 
 // Creating first standard grid 16x16
@@ -44,6 +42,50 @@ const etchFlexContElWidth = etchFlexContEl.offsetWidth;
 etchFlexContEl.style.height = `${etchFlexContElWidth}px`;
 
 
+// Changing between colors
+
+const colorBoxContEl = document.querySelector('.colors');
+let currentColor = '#fff';
+
+colorBoxContEl.addEventListener('click', e => {
+    const colorIdentifier = e.target.className.split(' ')[1];
+    switch(colorIdentifier) {
+        case 'color-box--1':
+            currentColor = 'var(--monaco-red)';
+        break;
+        case 'color-box--2':
+            currentColor = 'var(--monaco-white)';
+        break;
+        case 'color-box--3':
+            currentColor = 'var(--estonia-blue)';
+        break;
+        case 'color-box--4':
+            currentColor = 'var(--estonia-black)';
+        break;
+        case 'color-box--5':
+            currentColor = 'var(--finland-blue)';
+        break;
+        case 'color-box--6':
+            currentColor = 'var(--czech-red)';
+        break;
+        case 'color-box--7':
+            currentColor = 'var(--uk-dark-blue)';
+        break;
+        case 'color-box--8':
+            currentColor = 'var(--color-yellow)';
+        break;
+        case 'color-box--9':
+            currentColor = 'var(--color-orange)';
+        break;
+        case 'color-box--10':
+            currentColor = 'var(--color-purple)';
+        break;
+        case 'color-box--11':
+            currentColor = 'var(--color-green)';
+        break
+    }
+})
+
 // Color and Clear Grid
 
 const clearBtnEl = document.querySelector('#clearBtn');
@@ -52,9 +94,12 @@ etchFlexContEl.addEventListener('mouseover', colorAndClearGrid)
 function colorAndClearGrid(e) {
     let eTargetEl = e.target;
     if(eTargetEl.className === 'etch__box') {
-        eTargetEl.style.backgroundColor = 'var(--color-black)';
+        eTargetEl.style.backgroundColor = `${currentColor}`;
         clearBtnEl.addEventListener('click', e => {
             eTargetEl.style.backgroundColor = '';
         })
     }
 }
+
+
+// !!!!!!!!!! mouseover event napravi sa intervalima

443aec1a6cac876361d97f4e0eb24970e6749b5b
Emrah Isovic <emrahisovic20@gmail.com>
2024-03-20 17:52:59 +0100

    Create new grid when rangeBtnEl is clicked and new code layout

--- script.js
+++ script.js
@@ -2,16 +2,30 @@ const rootEl = document.querySelector('html');
 
 const rangeInputEl = document.querySelector('.range__input');
 const rangeValueEl = document.querySelector('.range__value');
+const rangeBtnEl = document.querySelector('.range-btn');
+
+console.log(rangeBtnEl);
 
 const etchFlexContEl = document.querySelector('.etch__flex-cont');
+
+// Creating first standard grid 16x16
+
+createGrid(16);
+
+// Changing value of rangeValueEl
 
 rangeInputEl.addEventListener('change', e => {
     rangeValueEl.textContent = `${e.target.value}x${e.target.value}`;
 })
 
-createGrid(16);
+
+// Create new grid onclick of rangeBtnEl based on users number of cells 
+rangeBtnEl.addEventListener('click', e => {
+    createGrid(rangeInputEl.value);
+})
 
 function createGrid(range){
+    etchFlexContEl.innerHTML = '';
     for(let i = 1; i <= range; i++) {
         const etchFlexRowEl = document.createElement('div');
         etchFlexRowEl.setAttribute('class', 'etch__flex-row');
@@ -24,12 +38,18 @@ function createGrid(range){
     }
 }
 
+//Adjusting height of etchFlexContEl
+
 const etchFlexContElWidth = etchFlexContEl.offsetWidth;
 etchFlexContEl.style.height = `${etchFlexContElWidth}px`;
+
+
+// Color and Clear Grid
+
 const clearBtnEl = document.querySelector('#clearBtn');
+etchFlexContEl.addEventListener('mouseover', colorAndClearGrid)
 
-
-etchFlexContEl.addEventListener('mouseover', e => {
+function colorAndClearGrid(e) {
     let eTargetEl = e.target;
     if(eTargetEl.className === 'etch__box') {
         eTargetEl.style.backgroundColor = 'var(--color-black)';
@@ -37,5 +57,4 @@ etchFlexContEl.addEventListener('mouseover', e => {
             eTargetEl.style.backgroundColor = '';
         })
     }
-})
-
+}

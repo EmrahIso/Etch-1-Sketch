bef5f824a3315fbc41ac5c77702f7a83eba7af5b
Emrah Isovic <emrahisovic20@gmail.com>
2024-03-21 20:47:09 +0100

    Functionality to mark used color, and random color game mode

--- script.js
+++ script.js
@@ -42,12 +42,32 @@ const etchFlexContElWidth = etchFlexContEl.offsetWidth;
 etchFlexContEl.style.height = `${etchFlexContElWidth}px`;
 
 
+// Random Color 
+
+const randomColorBtnEl = document.querySelector('#etch__random-colorBtn');
+let activateRandomMode = false;
+
+randomColorBtnEl.addEventListener('click', e => {
+    activateRandomMode = true;
+});
+
+function chooseRandomColor() {
+    randomNumber = Math.round(Math.random()*10);
+    console.log(randomNumber);
+}
+
 // Changing between colors
 
 const colorBoxContEl = document.querySelector('.etch__colors');
+const colorBoxEls = document.querySelectorAll('.etch__color-box');
 let currentColor = '';
 
 colorBoxContEl.addEventListener('click', e => {
+    colorBoxEls.forEach(colorBoxEl => {
+        colorBoxEl.classList.remove('is-used');
+    })
+    e.target.classList.add('is-used');
+    activateRandomMode = false;
     const colorIdentifier = e.target.className.split(' ')[1];
     switch(colorIdentifier) {
         case 'etch__color-box--1':
@@ -79,7 +99,7 @@ colorBoxContEl.addEventListener('click', e => {
         break;
         case 'etch__color-box--10':
             currentColor = 'var(--color-green)';
-        break
+        break;
     }
 })
 
@@ -91,6 +111,41 @@ etchFlexContEl.addEventListener('mouseover', colorAndClearGrid)
 function colorAndClearGrid(e) {
     let eTargetEl = e.target;
     if(eTargetEl.className === 'etch__box') {
+        if(activateRandomMode){
+            let randomNumber = Math.round(Math.random() * 10);
+            switch(randomNumber) {
+                case 1:
+                    currentColor = 'var(--monaco-red)';
+                break;
+                case 2:
+                    currentColor = 'var(--estonia-blue)';
+                break;
+                case 3:
+                    currentColor = 'var(--estonia-black)';
+                break;
+                case 3:
+                    currentColor = 'var(--finland-blue)';
+                break;
+                case 5:
+                    currentColor = 'var(--czech-red)';
+                break;
+                case 6:
+                    currentColor = 'var(--uk-dark-blue)';
+                break;
+                case 7:
+                    currentColor = 'var(--color-yellow)';
+                break;
+                case 8:
+                    currentColor = 'var(--color-orange)';
+                break;
+                case 9:
+                    currentColor = 'var(--color-purple)';
+                break;
+                case 10:
+                    currentColor = 'var(--color-green)';
+                break;
+            }
+        }
         eTargetEl.style.backgroundColor = `${currentColor}`;
         clearBtnEl.addEventListener('click', e => {
             eTargetEl.style.backgroundColor = '';
